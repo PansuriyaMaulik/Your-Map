bbed72623fb4c6ebe433352b01c6eb81cb09b656
PansuriyaMaulik <maulikpansuriya36@gmail.com>
2023-02-11 10:02:44 +0100

    Create the users route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const pinRoute = require('./routes/pins');
+const userRoute = require('./routes/users');
+
 
 const app = express()
 
@@ -14,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 }).catch((err) => console.log(err));
 
 app.use("/api/pins/", pinRoute)
+app.use("/api/users", userRoute)
 
 app.listen(8800, () => {
     console.log("Backend Server is running on port")
